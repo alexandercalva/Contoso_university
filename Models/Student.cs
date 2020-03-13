eab53a75f8eb0622b6b843a0421e89880123715b
calva-alexander <38995873+alexandercalva@users.noreply.github.com>
2020-03-13 18:21:16 -0500

    Inheritance
    Mapped inheritance to database
    Created the Person class
    Updated Instructor and Student
    Added Person to the model
    Created and update migrations
    Tested the implementation

--- Models/Student.cs
+++ Models/Student.cs
@@ -5,33 +5,14 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace ContosoUniversity.Models
 
 {
-    public class Student
+    public class Student : Person
     {
-        public int ID { get; set; }
-        [Required]
-        [StringLength(50, MinimumLength = 2)]
-        //[RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
-        [Display(Name = "Last Name")]
-        public string LastName { get; set; }
-        [Required]
-        [StringLength(50, MinimumLength = 2)]
-        // [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
-        [Column("FirstName")]
-        [Display(Name = "First Name")]
-        public string FirstName { get; set; }
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
 
+
         public ICollection<Enrollment> Enrollments { get; set; }
-        [Display(Name = "Full Name")]
-        public string FullName
-        {
-            get
-            {
-                return LastName + ", " + FirstName;
-            }
-        }
     }
 }
